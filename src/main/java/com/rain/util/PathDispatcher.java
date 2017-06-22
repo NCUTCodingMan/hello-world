d7bfdeb46dfa4c07634380cff9a4b25c95435bf4
NCUTCodingMan <1667254420@qq.com>
2017-06-22 19:59:22 +0800

    提交相关的路径控制方式,获取相关信息

---
+++ src/main/java/com/rain/util/PathDispatcher.java
@@ -0,0 +1,11 @@
+public class PathDispacther{
+	private Integer i;
+	public void setI(Integer i){
+		this.i = i ;
+	}
+	public static void main(String[] args){
+		PathDispatcher pd = new PathDispatcher();
+		pd.setI(1);
+		System.out.println(".......");
+	}
+}
